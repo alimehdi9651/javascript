1a7c1a354cdca9aa84ba3f1132631a5d0144071d
alimehdi <alimehdi9651@gmail.com>
2024-07-07 10:17:13 +0530

    revision

---
+++ loops.js
@@ -0,0 +1,101 @@
+//  node --watch filename.js 
+
+
+
+// for(i=10; i>=1; i--){
+//     console.log(i);   
+// }
+// console.log("------------");
+// let car = ["bmw","volvo", "Maruti"]
+// // for(i=0; i<=car.length; i++){
+// //     console.log(car[i])
+// // }
+// for(i=0; i<=20; i++){
+//     if(i%2===0){
+//         console.log(`number ${i} is even`);
+//     }
+// }
+// console.log("------------------");
+
+// for(i of car){ // "of" returns the exact value of list
+//     console.log(i);
+// }
+// console.log("-------------");
+// for(i in car){ // "in" returns the index of list
+//     console.log(car[i]);
+// // }
+// sum = 0
+// for(i=0; i<=20; i++){
+//     if(i%3===0 && i%5===0){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum);
+
+// while loop  
+
+// let i=0
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+
+// let i=0
+// let a=3
+// while(i<=10){
+//     console.log(i*a);
+//     i++
+// }
+
+
+// for(i=1; i<=10; i++){
+//       if(i === 3){
+//         break
+//       }  
+//       console.log(i);
+//     }
+//     console.log("-------------------");
+//     for(i=1; i<=10; i++){
+//         if(i === 3){
+//           continue
+//         }  
+//         console.log(i);
+      // }
+
+// let str = "alimehdi"
+// for(let i in str){
+//     console.log(`i = ${i }`);
+// }
+// // for of is use to access elements of a string and arrays
+// let arr = [1, 2, 3, 4]
+// for(let i in arr){
+//     console.log(i);
+// }
+
+
+
+// "for in" loop is used to access the keys of object and used is array too.
+// let student = {
+//       name : "ali",
+//       rollno : 23,
+//       pass : true,
+//   }
+//   for(let key in student){
+//       console.log(`keys = ${key} : value = ${student[key]}`);
+//   }
+
+// for(let i=0; i<=100; i++){
+//       console.log(i);
+//       i++;
+// }
+
+
+
+
+// let actnum = 25;
+// let usernum = prompt("enter a number");
+// while(actnum != usernum){
+//       usernum = prompt("u have entered wrong number ! please re-enter the number")
+// }
+// console.log("congragulation u entered a right numbe");
