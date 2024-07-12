bfdbf403330c32dc0d97eba82a94ef8cbb3209d5
alimehdi <alimehdi9651@gmail.com>
2024-07-12 19:58:27 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,15 @@
+//WINDOW object: it represent a open window in a brower. it is browser's object (not javascript's object)
+//its automatically created in the brower.
+//its a global object with lots of properties and methods.
+//----------------------------
+
+//Document object model: is use to perform some change in website dynamically 
+
+
+
+//Methods is DOM
+//1.) getElementById(id of tag)
+// id is represented by "#"
+let heading = document.getElementById("heading");
+console.log(heading)
+document.body.style.backgroundImage = 
